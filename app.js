6b716389ea7efa1a087c457ae7573b43d8d3d241
leandro923 <abetleandro15@gmail.com>
2025-06-15 18:03:46 -0300

    Delete app.js
    Signed-off-by: leandro923 <abetleandro15@gmail.com>

--- app.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker.controller) {
-console.log('[PWA Builder] active service worker found, no need to register')
-} else {
-navigator.serviceWorker.register('/sw.js', {
-scope: 'https://cookiielovee.netlify.app'
-}).then(function(reg) {
-console.log('Service worker has been registered for scope:'+ reg.scope);
-});
-}
